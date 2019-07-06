6e31a59d0d7b21190a8fa618b93f1c43dfa04e82
Tiandians <zhubaolin228@gmail.com>
2019-07-06 22:26:51 +0800

    做了一点题

--- Alguhrithm/2-1.c
+++ Alguhrithm/2-1.c
@@ -1,25 +1,22 @@
 #include <math.h>
 #include <stdio.h>
+_Bool is_daffodil(int wait)
+{
+    int a = wait / 100;
+    int b = wait / 10 % 10;
+    int c = wait % 10;
+    if(a*a*a+b*b*b+c*c*c==wait)
+        return 1;
+    else
+        return 0;
+}
 int main(void)
 {
-    int a = 1, b, c;
-    for (; a <= 9; a++)
+    for (int count = 100; count <= 999; count ++)
     {
-        //printf("round1\n");
-        if (a == 1)
-            b = 1;
-        for (; b <= 9; b++)
-        {
-            //printf("round2\n");
-            if(b == 1) 
-                c = 1;
-            for (; c <= 9; c++)
-            {
-                //printf("round3\n");
-                if (a * a * a + b * b * b + c * c * c >= 100 && a * a * a + b * b * b + c * c * c < 1000)
-                    printf("%d\n", a * a * a + b * b * b + c * c * c);
-            }
-        }
+        if(is_daffodil(count))
+            printf("%d\n", count);
     }
     return 0;
 }
+
