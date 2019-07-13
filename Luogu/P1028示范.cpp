2d20a7aea6d131990b4777527c21a98eb8f9eeae
Tiandians <zhubaolin228@gmail.com>
2019-07-13 21:23:08 +0800

    exe

---
+++ Luogu/P1028示范.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,cn=1,i,f[1001];
+	f[0]=f[1]=1;
+	cin>>n;
+	for(i=2;i<=n;i++)
+	{
+		if(i%2==0)
+			f[i]=f[i-1]+f[i/2];
+		else
+			f[i]=f[i-1];
+	}
+	cout<<f[n]<<endl;
+	return 0;
+}
