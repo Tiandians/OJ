f009f8ced9c947bbd4e838f03ea8c04f0001e86a
Tiandians <zhubaolin228@gmail.com>
2021-01-19 18:31:54 +0800

    1.19

--- CPrimerPlus/11.3.c
+++ CPrimerPlus/11.3.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <ctype.h>
 #define MAX 100
-int getw(char []);
+void chtg(char getto[]);
 
 int main(void)
 {
-    printf("Please enter some words.\n");
-    char get[MAX];
-    while(getw(get))
-    {
-    printf("here is what I get:%s\n", get);
-    }
+    char chr[MAX];
+    printf("Please enter some words and I will read the first one.\n");
+    chtg(chr);
+    puts(chr);
     return 0;
 }
 
-int getw(char getto[])
+void chtg(char getto[])
 {
-    
+    char temp;
+    int i = 0;
+    while(isblank(temp=getchar()))//循环跳过空格
+        ;
+    while((getto[i++]=temp)&&(!isblank(temp=getchar())))//循环：赋值、读取
+        ;
+    getto[i] = '\0';
+    while(getchar()!='\n')
+        ;
 }
